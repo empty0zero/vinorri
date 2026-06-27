34384d39be8dc2d172aebfd1b79826b894b05ea0
Алексей <alekseimaslov00@mail.ru>
2026-06-27 22:07:10 +0300

    Правка опять settings

--- Wedding_invitation/settings.py
+++ Wedding_invitation/settings.py
@@ -60,9 +60,8 @@ CSRF_TRUSTED_ORIGINS = [
 
 DATABASES = {
     "default": dj_database_url.config(
-        default=os.getenv("DATABASE_URL", "sqlite:///db.sqlite3"),
-        conn_max_age=600,
-        ssl_require=True
+        default=os.environ.get("DATABASE_URL"),
+        conn_max_age=600
     )
 }
 
